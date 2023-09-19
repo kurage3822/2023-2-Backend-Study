e49ce40002afcfde70c101b19b0b829495c84744
kurage3822 <108855087+kurage3822@users.noreply.github.com>
2023-09-19 21:30:18 +0900

    Week1 과제 제출

---
+++ Week1/HW/src/ALU.java
@@ -0,0 +1,9 @@
+public class ALU implements Calculator {
+    public int plus(int a, int b) { return a + b; }
+
+    public int minus(int a, int b) { return a - b; }
+
+    public int mul(int a, int b) { return a * b; }
+
+    public int div(int a, int b) { return a / b; }
+}
